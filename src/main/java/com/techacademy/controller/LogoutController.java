a0d607bb30fccdef885b9fcb1d202a3c98074ea1
koujimatsuda11 <koujimatsuda11@gmail.com>
2023-05-11 09:06:19 +0900

    ログアウトが動作しない問題の修正

--- src/main/java/com/techacademy/controller/LogoutController.java
+++ src/main/java/com/techacademy/controller/LogoutController.java
@@ -1,13 +1,13 @@
 package com.techacademy.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LogoutController {
 
     /** ログアウト処理を行なう */
-    @GetMapping("/logout")
+    @PostMapping("/logout")
     public String postLogout() {
         // ログイン画面にリダイレクト
         return "redirect:/login";
